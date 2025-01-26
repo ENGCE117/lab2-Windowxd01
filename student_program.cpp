8cd5a0a3cf5180ae318762fa181444686a748c72
Wave_ChangeTheWorld <133748311+Windowxd01@users.noreply.github.com>
2025-01-26 14:07:55 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse(char str1[]);
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
-}//end function
+    char text[50];
+    char *output;
+    output = reverse(text);
+    printf("MAIN : %s\n", output);
+    return 0;
+}
+
+char str2[50];
+
+char* reverse(char str1[]) {
+    int len;
+    printf("Enter a string: ");
+    gets(str1); // รับค่าสตริงจากผู้ใช้
+    len = strlen(str1);
+
+    for (int i = 0; i < len; i++) {
+        str2[i] = str1[len - 1 - i];
+    }
+    str2[len] = '\0'; // เพิ่ม null terminator
+
+    printf("REVERSE : %s\n", str2); // แสดงผลลัพธ์ในฟังก์ชัน reverse
+
+    return str2; // คืนค่าพอยน์เตอร์ไปยังสตริงที่กลับลำดับแล้ว
+}
